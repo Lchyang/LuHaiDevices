0fe63dd9a897729b0943f185bbe04cae027c40a2
Chen1996-1 <2435740021@qq.comm>
2020-07-27 22:27:51 +0800

    test

--- Device/views.py
+++ Device/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = 'Hello World'
+    print(context)
     return render(request, template_name='index.html', context=context, )
 
 
